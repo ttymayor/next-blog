6cf6049783fec5901a6a567968a00bb7d6e36d5a
ttymayor <lionhuang914903@gmail.com>
2025-11-16 00:32:32 +0800

    refactor: simplify Link components in Header by removing unnecessary prefetch attributes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,49 +14,29 @@ export default function Header() {
     <header className="mx-0 p-4 lg:mx-[10%]">
       <div className="mx-auto flex items-center justify-between">
         <h1 className="mb-0 text-2xl font-bold">
-          <Link
-            href="/"
-            className="text-foreground no-underline"
-            prefetch={false}
-          >
+          <Link href="/" className="text-foreground no-underline">
             市長/tantuyu
           </Link>
         </h1>
 
         <nav className="hidden space-x-4 md:flex">
           <Button variant="ghost" asChild>
-            <Link
-              href="/about"
-              className="text-foreground no-underline"
-              prefetch={false}
-            >
+            <Link href="/about" className="text-foreground no-underline">
               {pending ? <Spinner /> : "關於"}
             </Link>
           </Button>
           <Button variant="ghost" asChild>
-            <Link
-              href="/posts"
-              className="text-foreground no-underline"
-              prefetch={false}
-            >
+            <Link href="/posts" className="text-foreground no-underline">
               {pending ? <Spinner /> : "文章"}
             </Link>
           </Button>
           <Button variant="ghost" asChild>
-            <Link
-              href="/categories"
-              className="text-foreground no-underline"
-              prefetch={false}
-            >
+            <Link href="/categories" className="text-foreground no-underline">
               {pending ? <Spinner /> : "分類"}
             </Link>
           </Button>
           <Button variant="ghost" asChild>
-            <Link
-              href="/tags"
-              className="text-foreground no-underline"
-              prefetch={false}
-            >
+            <Link href="/tags" className="text-foreground no-underline">
               {pending ? <Spinner /> : "標籤"}
             </Link>
           </Button>
